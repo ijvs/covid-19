b1dab80ee4becb4d66d2ee0f0ec62c193ca81922
Gerardo Lucario <gmlukario@gmail.com>
2020-03-28 19:46:22 -0600

    Se busca el archivo que tiene los datos y se pasó a una clase

--- covid_stats.py
+++ covid_stats.py
@@ -3,74 +3,63 @@ import json
 import re
 from datetime import date
 
-
-def get_date_label():
-	url = "http://ncov.sinave.gob.mx/mapa.aspx"
-	req = requests.get(url)
-	try:
-		return re.findall(r'(Cierre con corte.+?)";',req.text)[-1]
-	except Exception as e:
-		return ""
-
-def get_stats(resource):
-	url = "http://ncov.sinave.gob.mx/Mapa45.aspx/Grafica"+resource
-	req = requests.post(url, data={}, headers= {"Content-Type":"application/json; charset=utf-8"})
-	
-	raw_data = json.loads(req.text)["d"]
-	result = json.loads(raw_data)
-	update_label = get_date_label();
-
-	
-	# if result is None:
-	# 	throws Exception(" error")
-
-	parse_data = {
-		"national_totals": {
-			"confirmed_cases": 0,
-			"suspicious_cases": 0,
-			"negative_cases":0,
-			"deads": 0,
-		},
-		"states_totals": [],
-		"update_label": update_label
-		};
-
-	for i in result:
-		state_total = {
-			"id": i[0],
-			"name": i[1],
-			"confirmed_cases": int(i[4]),
-			"negative_cases": int(i[5]),
-			"suspicious_cases": int(i[6]),
-			"deads": int(i[7])
-		}
-		parse_data["states_totals"].append(state_total)
-		parse_data["national_totals"]["confirmed_cases"] += state_total["confirmed_cases"]
-		parse_data["national_totals"]["suspicious_cases"] += state_total["suspicious_cases"]
-		parse_data["national_totals"]["negative_cases"] += state_total["negative_cases"]
-		parse_data["national_totals"]["deads"] += state_total["deads"]
-
-	return parse_data
-
-
-def get_last_stats():
-	initial_date = date(2020, 3, 27)
-	today = date.today()
-	delta = today-initial_date
-	last_resource = 23 + delta.days
-	intents = 3
-
-	for i in range(last_resource, last_resource-intents, -1):
+class DataToJson:
+	"""Class to convert government data format to beatiful JSON"""
+	def __init__(this):
+		this.url_mapa = "http://ncov.sinave.gob.mx/mapa.aspx"
+		this.req = requests.get(this.url_mapa)
+		this.headers = {"Content-Type":"application/json; charset=utf-8"}
+			
+	def get_date_label(this):
+		"""Get info label about last updete"""
 		try:
-			print(i)
-			return get_stats(str(i))
+			return re.findall(r'(Cierre con corte.+?)";',this.req.text)[-1]
 		except Exception as e:
-			continue
+			return ""
 
-	return {
-		"error": "Not data found."
-	}
+	def get_source_file_number(this):
+		"""Get name of the source file"""
+		try:
+			return re.findall(r'Mapa45.aspx\/(.+?)",',this.req.text)[0]
+		except Exception as e:
+			return ""		
 
+	def get_stats(this):
+		"""Return the formated data"""
+		url = "http://ncov.sinave.gob.mx/Mapa45.aspx/"+this.get_source_file_number()
+		req = requests.post(url, data={}, headers= this.headers)
+		
+		raw_data = json.loads(req.text)["d"]
+		result = json.loads(raw_data)
+		update_label = this.get_date_label();
 
-print(get_last_stats())
+		parse_data = {
+			"national_totals": {
+				"confirmed_cases": 0,
+				"suspicious_cases": 0,
+				"negative_cases":0,
+				"deads": 0,
+			},
+			"states_totals": [],
+			"update_label": update_label
+			};
 
+		for i in result:
+			state_total = {
+				"id": i[0],
+				"name": i[1],
+				"confirmed_cases": int(i[4]),
+				"negative_cases": int(i[5]),
+				"suspicious_cases": int(i[6]),
+				"deads": int(i[7])
+			}
+			parse_data["states_totals"].append(state_total)
+			parse_data["national_totals"]["confirmed_cases"] += state_total["confirmed_cases"]
+			parse_data["national_totals"]["suspicious_cases"] += state_total["suspicious_cases"]
+			parse_data["national_totals"]["negative_cases"] += state_total["negative_cases"]
+			parse_data["national_totals"]["deads"] += state_total["deads"]
+
+		return parse_data
+
+obj = DataToJson()
+print(obj.get_stats())
