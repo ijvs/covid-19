0d42f6a1a314593ca32621ace833bbd30f64ac62
Jonathan Velazquez <ijonathanvs@gmail.com>
2020-04-03 16:53:34 -0600

    Use https instead of http

--- covid_stats.py
+++ covid_stats.py
@@ -6,7 +6,7 @@ from datetime import date
 class DataToJson:
 	"""Class to convert government data format to beatiful JSON"""
 	def __init__(this):
-		this.url_mapa = "http://ncov.sinave.gob.mx/mapa45.aspx"
+		this.url_mapa = "https://ncov.sinave.gob.mx/mapa.aspx"
 		this.req = requests.get(this.url_mapa)
 		this.headers = {"Content-Type":"application/json; charset=utf-8"}
 			
@@ -20,19 +20,20 @@ class DataToJson:
 	def get_source_file_number(this):
 		"""Get name of the source file"""
 		try:
-			return re.findall(r'Mapa45.aspx\/(.+?)",',this.req.text)[0]
+			return re.findall(r'Mapa.aspx\/(.+?)",',this.req.text)[0]
 		except Exception as e:
 			return ""		
 
 	def get_stats(this):
 		"""Return the formated data"""
-		url = "http://ncov.sinave.gob.mx/Mapa45.aspx/"+this.get_source_file_number()
+		url = "https://ncov.sinave.gob.mx/Mapa.aspx/"+this.get_source_file_number()
 		req = requests.post(url, data={}, headers= this.headers)
-		
+
 		raw_data = json.loads(req.text)["d"]
 		result = json.loads(raw_data)
 		update_label = this.get_date_label();
 
+		print(update_label)
 		parse_data = {
 			"national_totals": {
 				"confirmed_cases": 0,
